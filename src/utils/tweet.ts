c1670fdd199f260b6d9744af7fe7a1727cdef056
imamrmdn <imamramadhan420@gmail.com>
2024-12-24 17:04:01 +0700

    modified tweet

--- src/utils/tweet.ts
+++ src/utils/tweet.ts
@@ -1,34 +1,34 @@
 export const tweetsData = [
   {
     tweet:
-      'Learning, gaming, and glowing in a world of neon. ✨📖🎮 Let the cyberpunk adventure begin! #FutureTech',
+      'Learning, gaming, and glowing in a world of neon. \n\n✨📖🎮 Let the cyberpunk adventure begin!',
   },
   {
     tweet:
-      "The future is at their fingertips, with a cyborg student's mind and hands ready for the digital age. 🤖💻 #AnimeArt",
+      "The future is at their fingertips, with a cyborg student's mind and hands ready for the digital age.",
   },
   {
     tweet:
-      'Late nights, glowing screens, and dreams of a new world. 🌃💡 The cyberpunk student life is here! #NeonVibes',
+      'Late nights, glowing screens, and dreams of a new world. \n\n🌃💡 The cyberpunk student life is here!',
   },
   {
     tweet:
-      'In a neon-lit world, knowledge is the greatest power. ⚡📚 Follow the journey of a student cyborg. #CyberpunkArt',
+      'In a neon-lit world, knowledge is the greatest power. \n\n⚡📚 Follow the journey of a student cyborg.',
   },
   {
     tweet:
-      'By day they study, by night they explore virtual realms. 🌌💻 The double life of a cyberpunk student! #AnimeLife',
+      'By day they study, by night they explore virtual realms. \n\n🌌💻 The double life of a cyberpunk student!',
   },
   {
     tweet:
-      'Tech meets tradition in the hands of this cyberpunk student, changing what it means to learn. 🤖✨ #FuturisticVibes',
+      'Tech meets tradition in the hands of this cyberpunk student, changing what it means to learn 🤖✨',
   },
   {
     tweet:
-      'Neon skies and limitless potential await. The future is bright for this student cyborg! 🌆💻 #CyberpunkDreams',
+      'Neon skies and limitless potential await. The future is bright for this student cyborg 🌆💻',
   },
   {
     tweet:
-      'A cyborg with a backpack and a vision. 🎧📚 Ready to explore and conquer the digital frontier! #AnimeArt',
+      'A cyborg with a backpack and a vision. \n\n🎧📚 Ready to explore and conquer the digital frontier!',
   },
 ];
