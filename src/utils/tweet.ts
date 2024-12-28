91f6b0a2ed9f3d1dd42c20caab750ea3b23f9f64
imamrmdn <imamramadhan420@gmail.com>
2024-12-28 18:51:17 +0700

    update tweet

--- src/utils/tweet.ts
+++ src/utils/tweet.ts
@@ -1,34 +1,6 @@
 export const tweetsData = [
   {
     tweet:
-      'Made a perfect latte today. Too bad the stars don’t care about my foam art.',
-  },
-  {
-    tweet:
-      'The telescope isn’t big enough to see my dreams, but it’s good enough to remind me they’re out there.',
-  },
-  {
-    tweet:
-      'A customer asked if I’m always this sarcastic. I told them, "Only when I’m awake."',
-  },
-  {
-    tweet:
-      'Staring at the stars reminds me of how small I am, but also how much there is to explore. One day...',
-  },
-  {
-    tweet:
-      'Someone left their phone number in the tip jar. Sweet gesture, but I’m already in a relationship with caffeine.',
-  },
-  {
-    tweet:
-      'Every latte I make feels like a tiny piece of art. Too bad art doesn’t pay the bills.',
-  },
-  {
-    tweet:
-      'The night sky feels like a secret too big to keep but too beautiful to share.',
-  },
-  {
-    tweet:
       'Left an anonymous note today: "You’re capable of more than you think." Hope it helps someone, even if I’m not taking my own advice.',
   },
   {
